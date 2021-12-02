2fe1043abe576dd2dd3dbe142a43f6ab1a32a638
Maya Galili <33996691+MayaGalili@users.noreply.github.com>
2021-12-02 23:35:48 +0200

    wrap code as function

--- resetRowsWithZeros.py
+++ resetRowsWithZeros.py
@@ -11,31 +11,33 @@ Created on Tue May 22 15:03:36 2018
 
 import numpy as np
 
-# create random binary matrix
-MAT_SZ = 25
-v1=np.zeros(MAT_SZ)
-v2=np.zeros(MAT_SZ)
-M=np.random.randint(2,size=(MAT_SZ,MAT_SZ))
+def main(mat_size):
+    # create random binary matrix
+    v1=np.zeros(mat_size)
+    v2=np.zeros(mat_size)
+    M=np.random.randint(2,size=(mat_size,mat_size))
 
-print('TEST MATRIX')
-print(M)
+    print('TEST MATRIX')
+    print(M)
 
-v1 = np.sum(M,axis=1)
-v2 = np.sum(M,axis=0)    
+    v1 = np.sum(M,axis=1)
+    v2 = np.sum(M,axis=0)    
 
-print('V1')
-print(v1)
-print('V2')
-print(v2)
+    print('V1')
+    print(v1)
+    print('V2')
+    print(v2)
 
-# zero out rows and columns containig zeros
-for i in range(0,MAT_SZ):
-    if (v1[i] < MAT_SZ):
-        M[i,:] = 0
-    if (v2[i] < MAT_SZ):
-        M[:,i] = 0    
-        
-print('RESULTS')
-print(M)
+    # zero out rows and columns containig zeros
+    for i in range(0,mat_size):
+        if (v1[i] < mat_size):
+            M[i,:] = 0
+        if (v2[i] < mat_size):
+            M[:,i] = 0    
 
-    
+    print('RESULTS')
+    print(M)
+
+if __main__ == "__name__":
+    MAT_SZ = 25
+    main(MAT_SZ)   
